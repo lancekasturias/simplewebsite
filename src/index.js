804ad50d40226ac09856c16c43be67eb73f82f24
lancekasturias <70435588+lancekasturias@users.noreply.github.com>
2021-02-02 15:24:01 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+ReactDOM.render(
+  <div>
+    <center>
+      <h1>Top beaches destinations in Philippines</h1>
+    </center>
+
+    <h2>Boracay</h2>
+    <p>
+      Philippine island known for White Beach, Bulabog Beach water sports &
+      Mount Luho views.
+    </p>
+
+    <h2>El Nido</h2>
+    <p>
+      Philippine municipality known for its beaches, dive sites & the islands of
+      the Bacuit archipelago.
+    </p>
+
+    <h2> Coron </h2>
+    <p> Scuba, Diving, lagoons, lakes and beaches</p>
+
+    <h2> Puerto Princesa</h2>
+    <p>
+      Filipino city on Palawan island known for Puerto Princesa Subterranean
+      River and scuba diving{" "}
+    </p>
+
+    <h2>Cebu</h2>
+    <p>Vibrant island with Cebu city and beaches </p>
+  </div>,
+  document.getElementById("root")
+);
